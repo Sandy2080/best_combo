79c2f00d8d9d00c9fed3b29532cc73889da84f20
Sandy Ludosky <sludosky@xebia.fr>
2023-05-12 15:50:29 +0200

    algo fix

--- optimized.py
+++ optimized.py
@@ -6,7 +6,7 @@ import time
 from itertools import combinations
 
 start_time = time.time()
-MAX_INVEST = 5001
+MAX_INVEST = 500
 
 
 def main():
@@ -32,8 +32,8 @@ def main():
         cost.append(share[1])
         profit.append(share[2])
 
-    knapSack(max_invest, cost, profit, shares_total, shares_list)
-
+    best_combo = knapSack(max_invest, cost, profit, shares_total, shares_list)
+    display_results(best_combo)
     return 0
 
 # optimize
@@ -44,12 +44,13 @@ def main():
 
 
 def knapSack(max_invest, cost, profit, shares_total, shares_list):
-    """(ks) for 0-1 knapsack problem
-     @param shares_list: max_invest (max capacity), cost (weight), profit (values), shares_total, shares_list
-     @return: best combinaition and best return on investment
+    """Initialize the matrix (ks) for 0-1 knapsack problem
+     Get best shares combination
+
+     @param shares_list: shares data (list)
+     @return: best possible combination (list)
     """
 
-    #
     # TABLE with actions list and max investissement
     ks = [[0 for x in range(max_invest + 1)] for x in range(shares_total + 1)]
 
@@ -60,41 +61,28 @@ def knapSack(max_invest, cost, profit, shares_total, shares_list):
             # while cost < W or max invest, add action to table or knapsack
             if cost[i-1] <= w:
                 # Get max profit bet.
-                # 1- get max profit for every combo
-                # 2- if cost not exceeding the max invest (max capacity)
+                # 1- if weight not exceeding the max invest (max capacity)
+                # 2- return max profit from previous object
                 ks[i][w] = max(ks[i-1][w], ks[i-1][w-cost[i-1]] + profit[i-1])
             else:
+                # if no better max profit is found, returns previous max profit
                 ks[i][w] = ks[i-1][w]
 
-    print(f"\nBest investment : {ks[shares_total][max_invest]}€")
-    print("\nTime elapsed : ", time.time() - start_time, "seconds\n")
-
-
-# bruteforce
-
-
-def brute_force():
-
-    # read csv
-    shares_list = read_csv()
-    best_profit = 0
+    # Retrieve combination of shares from optimal profit
+    # 1- retrieve max and best profit in the table
     best_combo = []
-    # generate all combos
-    for i in range(len(shares_list)):
-        combos = combinations(shares_list, i+1)
 
-        # combos = get_combos(shares_list)
-        # filter combos - must be < 500E + calculate profits
-        combos = [combo
-                  for combo in combos if calc_cost(combo) <= 500]
+    while max_invest >= 0 and shares_total >= 0:
 
-        # return best profit
-        for combo in combos:
-            total_profit = calc_profit(combo)
-            if total_profit > best_profit:
-                best_profit = total_profit
-                best_combo = combo
-    display_results(best_combo)
+        if ks[shares_total][max_invest] == \
+                ks[shares_total-1][max_invest - cost[shares_total-1]] + profit[shares_total-1]:
+
+            best_combo.append(shares_list[shares_total-1])
+            max_invest -= cost[shares_total-1]
+
+        shares_total -= 1
+
+    return best_combo
 
 
 def get_combos(shares):
@@ -139,27 +127,43 @@ def read_csv(filename):
 
 
 def calc_cost(combo):
+    """Sum of current share combo profit
+    @param combo: list of shared combo
+    @return: total costs
+    """
 
-    prices = []
+    costs = []
     for el in combo:
-        prices.append(el[1])
+        costs.append(el[1] / 100)
 
-    return sum(prices)
+    return sum(costs)
 
 # calcul retour sur investissement
 
 
 def calc_profit(combo):
+    """Sum of current share combo profit
+    @param combo: list of shared combo
+    @return: total profits
+    """
+
     profits = []
     for el in combo:
-        profits.append(el[1] * el[2] / 100)
+        profits.append(el[2])
 
     return sum(profits)
 
 
 def display_results(best_combo):
-    print(f"Best investment : {calc_profit(best_combo)}€")
-    print("=======================================")
+    """Display best combination results with costs, profits and time elapsed
+    @param best_combo: most profitable shares combination
+    """
+    print(f"\nMost profitable investment ({len(best_combo)} shares) :\n")
+    for item in best_combo:
+        print(f"{item[0]} | {item[1] / 100} € | +{item[2]} €")
+
+    print("\nTotal cost : ", calc_cost(best_combo), "€")
+    print("Profit after 2 years : +", calc_profit(best_combo), "€")
     print("\nTime elapsed : ", time.time() - start_time, "seconds\n")
 
 
